db249da0dfbb7b12743dc31d18293d0e420da90a
Dylan Mozlowski <dylan.mozlowski@gmail.com>
2026-02-02 20:03:32 -0800

    add tool to show inferred type for return type

--- src/output-issue.ts
+++ src/output-issue.ts
@@ -1,14 +1,43 @@
-import { generateText, type ModelMessage } from "ai";
+import { generateText, tool, type ModelMessage } from "ai";
+import z from "zod";
 
 export const makeLLMRequest = async ({
   messages,
 }: {
   messages: Array<ModelMessage>;
 }) => {
+  const getWeather = tool({
+    inputSchema: z.object({
+      location: z.string(),
+    }),
+    execute: () => "Sunny",
+  });
+
   const result = await generateText({
     model: "anthropic/claude-sonnet-4.5",
+    tools: {
+      getWeather,
+    },
     messages,
   });
 
   return result;
 };
+
+/**
+ * In v5, `result` has the type:
+ *
+ * GenerateTextResult<{
+ *   getWeather: Tool<{
+ *       location: string;
+ *   }, string>;
+ * }, never>
+ */
+const result = await makeLLMRequest({
+  messages: [
+    {
+      role: "user",
+      content: "What's the weather like in San Francisco?",
+    },
+  ],
+});
